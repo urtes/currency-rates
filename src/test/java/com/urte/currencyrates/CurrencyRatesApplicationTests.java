a3c174e563899b9c32c35ce60686c8417607c29d
Urte <urte.rimsaite@gmail.com>
2019-12-20 12:02:40 +0200

    Initial commit

---
+++ src/test/java/com/urte/currencyrates/CurrencyRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urte.currencyrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
