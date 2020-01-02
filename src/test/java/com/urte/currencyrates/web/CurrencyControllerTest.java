f83b5143f2491bdad43d5d17e885c40f1f75e73c
Urte <urte.rimsaite@gmail.com>
2020-01-02 15:24:51 +0200

    Add test for Controller getHistory()

--- src/test/java/com/urte/currencyrates/web/CurrencyControllerTest.java
+++ src/test/java/com/urte/currencyrates/web/CurrencyControllerTest.java
@@ -4,6 +4,7 @@ import com.urte.currencyrates.data.CurrencyRepository;
 import com.urte.currencyrates.domain.CurrencyByDate;
 import com.urte.currencyrates.service.CurrencyService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -38,5 +40,16 @@ public class CurrencyControllerTest {
                 .andExpect(model().attributeExists("currencies"))
                 .andExpect(view().name("rates"));
         verify(mockCurrencyRepository, times(1)).getAllForToday();
+    }
+
+    @Test
+    public void testGetHistory() throws Exception {
+        ArgumentCaptor<String> codeCaptor = ArgumentCaptor.forClass(String.class);
+        mockMvc.perform(get("/history/AUD"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("history"))
+                .andExpect(view().name("history"));
+        verify(mockCurrencyRepository, times(1)).findAllByCodeOrderByDateDesc(codeCaptor.capture());
+        assertEquals("AUD", codeCaptor.getValue());
     }
 }
